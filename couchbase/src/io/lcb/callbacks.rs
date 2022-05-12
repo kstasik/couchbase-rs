f85498d8b42c65bf4094449d8021e68260636d36
Kacper Stasik <kacper@stasik.eu>
2022-05-12 14:15:46 +0200

    chore: asd

--- couchbase/src/io/lcb/callbacks.rs
+++ couchbase/src/io/lcb/callbacks.rs
@@ -747,7 +747,7 @@ pub unsafe extern "C" fn logger_callback(
         _ => log::Level::Error,
     };
 
-    let mut target_buffer = [0u8; LOG_MSG_LENGTH];
+    let mut target_buffer = [0i8; LOG_MSG_LENGTH];
     let result = wrapped_vsnprintf(
         &mut target_buffer[0],
         LOG_MSG_LENGTH as c_uint,
